227112ee76937bd5a85b82c168c96c1c3b206bd0
Jon West <ddproxy@gmail.com>
2014-02-23 23:31:51 +0000

    wqIndex configuration to pull proper port and files

--- public/admin.js
+++ public/admin.js
@@ -24,7 +24,7 @@ window.onload = function() {
 		} else {
 			var reply = false;
 			alert("Authentication failed");
-            window.location = "../";
+//            window.location = "../";
 		}
 	});
 	
